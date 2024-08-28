77a33cb49f9bd2ff17b445669f674c6d4059868e
mahnoor77 <mahnoorwattoo77@gmail.com>
2024-08-28 10:40:20 +0500

    final

---
+++ main.js
@@ -0,0 +1,11 @@
+
+var addEmployeeModal= document.querySelector("#employee-modal");
+addEmployeeModal.style.display="none";
+
+function addEmployee(){
+    addEmployeeModal.style.display="none";
+}
+
+function showAddEmpModal(){
+    addEmployeeModal.style.display="block";
+}
